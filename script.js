0af611e6d98010c35d8066f37f287ac50e364862
zuzOup <zuzanaoupicka@gmail.com>
2024-03-06 10:09:34 -0500

    .

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ console.log(
   "width " + window.innerWidth + "px"
 );
 
+const wait = (ms) => new Promise((res) => setTimeout(res, ms));
+
 /*header disapearing on scroll*/
 
 const header = document.getElementById("header");
@@ -30,104 +32,49 @@ window.addEventListener("scroll", () => {
 /*word rotate*/
 
 const changeWord = document.getElementById("article_title_span");
-const wait = (ms) => new Promise((res) => setTimeout(res, ms));
 
-async function rotateWords() {
+function rotateWords() {
+  const rotate = {
+    content: ["CSS", "JS", "REACT", "HTML"],
+    time1: 2000,
+    time2: 1000,
+  };
+
   function removeExtra() {
-    if (
-      Array.from(changeWord.getElementsByClassName("article_title_span_middle"))
-        .length > 1
-    ) {
-      const toBeRemoved = Array.from(
-        changeWord.getElementsByClassName("article_title_span_middle")
-      )[0];
-      toBeRemoved.remove();
-    }
+    const arr = Array.from(
+      changeWord.getElementsByClassName("article_title_span_middle")
+    );
+    if (arr.length > 1) arr[0].remove();
   }
-  removeExtra();
 
-  const js = Array.from(
-    changeWord.getElementsByClassName("article_title_span_middle")
-  )[0];
+  let i = 0;
 
-  const html = document.createElement("span");
-  html.classList.add("article_title_span_under");
-  html.classList.add("article_title_span_html");
+  setInterval(async () => {
+    i + 1 === rotate.content.length ? (i = 0) : i++;
 
-  html.innerHTML = "HTML";
-  changeWord.append(html);
+    const rotStart = Array.from(
+      changeWord.getElementsByClassName("article_title_span_middle")
+    )[0];
 
-  await wait(2000);
+    const rotEnd = document.createElement("span");
+    rotEnd.classList.add("article_title_span_under");
+    rotEnd.classList.add(`article_title_span_${i}`);
+    rotEnd.innerHTML = rotate.content[i];
 
-  js.classList.remove("article_title_span_middle");
-  js.classList.add("article_title_span_top");
+    changeWord.append(rotEnd);
 
-  html.classList.remove("article_title_span_under");
-  html.classList.add("article_title_span_middle");
+    await wait(rotate.time1);
 
-  await wait(1000);
-  js.remove();
-  removeExtra();
+    rotStart.classList.remove("article_title_span_middle");
+    rotStart.classList.add("article_title_span_top");
 
-  const css = document.createElement("span");
-  css.classList.add("article_title_span_under");
-  css.classList.add("article_title_span_css");
-  css.innerHTML = "CSS";
+    rotEnd.classList.remove("article_title_span_under");
+    rotEnd.classList.add("article_title_span_middle");
 
-  changeWord.append(css);
-
-  await wait(2000);
-
-  html.classList.remove("article_title_span_middle");
-  html.classList.add("article_title_span_top");
-
-  css.classList.remove("article_title_span_under");
-  css.classList.add("article_title_span_middle");
-
-  await wait(1000);
-
-  html.remove();
-  removeExtra();
-
-  const react = document.createElement("span");
-  react.classList.add("article_title_span_under");
-  react.classList.add("article_title_span_react");
-  react.innerHTML = "REACT";
-
-  changeWord.append(react);
-
-  await wait(2000);
-
-  css.classList.remove("article_title_span_middle");
-  css.classList.add("article_title_span_top");
-
-  react.classList.remove("article_title_span_under");
-  react.classList.add("article_title_span_middle");
-
-  await wait(1000);
-  css.remove();
-  removeExtra();
-
-  const js2 = document.createElement("span");
-  js2.classList.add("article_title_span_under");
-  js2.classList.add("article_title_span_js");
-  js2.innerHTML = "JS";
-
-  changeWord.append(js2);
-
-  await wait(2000);
-
-  react.classList.remove("article_title_span_middle");
-  react.classList.add("article_title_span_top");
-
-  js2.classList.remove("article_title_span_under");
-  js2.classList.add("article_title_span_middle");
-
-  await wait(1000);
-
-  react.remove();
-  removeExtra();
+    await wait(rotate.time2);
+    rotStart.remove();
+    removeExtra();
+  }, rotate.time1 + rotate.time2);
 }
 
 rotateWords();
-setInterval(rotateWords, 12000);
