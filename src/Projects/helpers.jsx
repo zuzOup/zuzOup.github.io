1dfc1dfc6c59f5e4d1c9a043734514a163eb7cf7
Zuzana Oupická <zuzanaoupicka@gmail.com>
2024-06-25 20:47:25 -0400

    Update helpers.jsx

--- src/Projects/helpers.jsx
+++ src/Projects/helpers.jsx
@@ -1,10 +1,4 @@
-// const auth = {
-//   method: "GET",
-//   headers: {
-//     Authorization:
-//       "token gEiEthub_pat_11A4IT67Y0R5CGahaviimi_s1s5yJoA7RKhtMUtc7CdY9XoI4CKoXcm9Y9BZyGCkWdOYYPYDV5waqe83A0",
-//   },
-// };
+
 
 export async function fetchData(url, setter, conditions) {
   await fetch(url)
